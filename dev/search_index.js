676d985c5f747c9aaa33efdd4971803402e0bfda
Documenter.jl <documenter@juliadocs.github.io>
2023-12-11 23:33:00 +0000

    build based on 6667bcc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScoreMatchingVI","category":"page"},{"location":"#ScoreMatchingVI","page":"Home","title":"ScoreMatchingVI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScoreMatchingVI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScoreMatchingVI]","category":"page"}]
+}
